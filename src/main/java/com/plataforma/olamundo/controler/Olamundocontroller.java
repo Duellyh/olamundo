6aa2a4dd0ccb1421a54771388d92206e10941cc3
Duellyhermenson Menezes <duellyhermenson@hotmail.com>
2024-07-17 00:17:29 -0300

    upload

---
+++ src/main/java/com/plataforma/olamundo/controler/Olamundocontroller.java
@@ -0,0 +1,17 @@
+package com.plataforma.olamundo.controler;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+
+public class Olamundocontroller {
+
+
+    @GetMapping("/")   
+    public String OlaMundo(){
+        return "Ola mundo Vscode SOU O MELHOR ";
+    }
+}
